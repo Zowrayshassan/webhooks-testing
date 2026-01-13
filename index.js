6b7d8424e3c563bb4607797a2bcd1d6873dda238
Zowrays Hassan <zowrayshassan@gmail.com>
2026-01-13 19:01:32 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+// Test route
+app.get("/", (req, res) => res.send("AgriPro Backend Running!"));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
